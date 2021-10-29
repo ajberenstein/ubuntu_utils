60bed0b636c512766833251a313e51a953173ffd
ajberenstein <arieljberenstein@gmail.com>
2021-10-29 15:59:00 -0300

    add personalized rstudio version managment

--- server_config.sh
+++ server_config.sh
@@ -16,11 +16,11 @@ echo \
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
+sudo docker run hello-world  #veryf installation
 sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
-docker run hello-world
+newgrp docker 
+docker run hello-world 
 
 
 #################################
@@ -35,3 +35,10 @@ sudo apt-get install r-base
 sudo apt-get install gdebi-core
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2021.09.0-351-amd64.deb
 sudo gdebi rstudio-server-2021.09.0-351-amd64.deb
+sudo rstudio-server verify-installation # verify instalation
+# custom launcher with advanced version managment (and istallation)
+wget https://github.com/ajberenstein/ubuntu_utils/blob/main/launch_rstudio_server.sh
+sudo chmod +x launch_rstudio_server.sh
+sudo mv launch_rstudio_server.sh /usr/bin/
+
+
