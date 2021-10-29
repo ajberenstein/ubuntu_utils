b47dce08659fca3a1fd3cdc406e7e745a1197ca2
ajberenstein <arieljberenstein@gmail.com>
2021-10-29 16:11:20 -0300

    add miniconda install (interactive)

--- server_config.sh
+++ server_config.sh
@@ -23,10 +23,6 @@ newgrp docker
 docker run hello-world 
 
 
-#################################
-# Miniconda
-#################################
-
 
 #################################
 # Rstudio server 
@@ -42,3 +38,10 @@ sudo chmod +x launch_rstudio_server.sh
 sudo mv launch_rstudio_server.sh /usr/bin/
 
 
+#################################
+# Miniconda
+#################################
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
+check="935d72deb16e42739d69644977290395561b7a6db059b316958d97939e9bdf3d"
+sudo bash Miniconda3-latest-Linux-x86_64.sh
+conda init fish
