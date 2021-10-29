3903c2a069a52cbdef3c48631a4d8264e780f51f
ajberenstein <arieljberenstein@gmail.com>
2021-10-29 18:02:08 -0300

    add Dropbox installation

--- server_config.sh
+++ server_config.sh
@@ -45,3 +45,10 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
 check="935d72deb16e42739d69644977290395561b7a6db059b316958d97939e9bdf3d"
 sudo bash Miniconda3-latest-Linux-x86_64.sh
 conda init fish
+
+
+################################
+## Dropbox
+################################
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+~/.dropbox-dist/dropboxd
