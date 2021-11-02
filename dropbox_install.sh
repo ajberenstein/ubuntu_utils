d8ba5e79223d3f748fd761d91487d87ccb78ba4b
ajberenstein <arieljberenstein@gmail.com>
2021-11-02 10:48:54 -0300

    add command line functionality

--- dropbox_install.sh
+++ dropbox_install.sh
@@ -3,3 +3,6 @@
 ################################
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 ~/.dropbox-dist/dropboxd
+wget https://www.dropbox.com/download?dl=packages/dropbox.py -O /home/$USER/dropbox.py
+chmod +x /home/$USER/dropbox.py
+sudo mv /home/$USER/dropbox.py /usr/bin/
